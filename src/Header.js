ec3c4c1c9c0e0e79cbd0e58b018a43880493795c
roshan-nn <roshan.thomas767@gmail.com>
2025-12-10 19:13:04 +0530

    srtyled

---
+++ src/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import'./Header.css'
+const ROSHAN = () => {
+  return (
+    <div className='head'>
+        <h1>header</h1> 
+    </div>
+  )
+}
+
+export default ROSHAN;
---
+++ src/Name.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './Name.css';
+
+
+const Name = () => {
+  return (
+    <div className="initial">
+        <h1>Name</h1>
+        </div>
+  )
+}
+
+export default Name
